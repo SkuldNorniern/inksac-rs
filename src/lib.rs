38327cbb0b91b48ae8fce98b269299a80da1f6d2
SkuldNorniern <skuldnorniern@gmail.com>
2023-05-31 07:00:31 +0000

    docs: add simple example code

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,40 @@
+//! Component Based Terminal Colorization
+//! 
+//! # Usage Steps
+//!
+//! 1. Predefine your [Styles](crate::types::Style)
+//! 2. Create a [ColoredString](crate::types::ColoredString) with the predefined style
+//! 3. print the ColoredString just like using a normal String 
+//!
+//!
+//! # Example
+//!
+//!
+//! ```rust
+//! use inksac::check_color_available;
+//! use inksac::types::*;
+//!
+//! match check_color_available() {
+//!     Ok(_) => println!("Terminal supports ANSI colors"),
+//!     Err(_) => println!("Terminal does not support ANSI colors"),
+//! }
+//!
+//! // Step 1
+//! const TITLESTYLE: Style = Style{
+//!     forground: Some(Color::Green),
+//!     background: Some(Color::Red),
+//! };
+//! // Step 2
+//! let title_text: ColoredString = ColoredString::new(
+//!     "Hello World".to_string(),
+//!     TITLESTYLE
+//! );
+//!
+//! // Step 3
+//! println!("{}", title_text);
+//! ```
+
 mod ansi_base;
 pub mod types;
 
-pub use ansi_base::*;
+pub use ansi_base::check_color_available;
