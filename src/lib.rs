8e1444c585aa5b262f4635df049dbd8a7fff8b1c
SkuldNorniern <skuldnorniern@gmail.com>
2023-09-14 09:39:07 +0900

    docs: update main lib docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
-//! Component Based Terminal Colorization
+//! Component-Based Terminal Colorization Library
 //!
-//! # Usage Steps
+//! This crate facilitates the coloring of terminal text output through a component-based system.
+//! You can define various styles and apply them to strings, then print them to the terminal,
+//! all while enjoying type safety and composability.
 //!
-//! 1. Predefine your [Styles](crate::types::Style)
-//! 2. Create a [ColoredString](crate::types::ColoredString) with the predefined style
-//! 3. print the ColoredString just like using a normal String
+//! ## Steps to Use
 //!
+//! 1. **Predefine Your Styles**: Before anything else, set up your styles using [`Style`](crate::types::Style) or with the builder pattern using [`StyleBuilder`](crate::types::StyleBuilder).
+//! 2. **Create Colored Strings**: Using the predefined styles, create colored strings with [`ColoredString`](crate::types::ColoredString).
+//! 3. **Print the Colored String**: Print the `ColoredString` instances just as you would with regular strings.
 //!
-//! # Example
+//! ## Example
 //!
+//! Below is an example that demonstrates the usage of this crate, including utilizing the builder pattern for creating styles:
 //!
 //! ```rust
 //! use inksac::is_color_available;
@@ -19,25 +23,25 @@
 //!     Err(_) => println!("Terminal does not support ANSI colors"),
 //! }
 //!
-//! // Step 1
-//! const TITLESTYLE: Style = Style{
-//!     foreground: Some(Color::Green),
-//!     background: Some(Color::Red),
-//!     bold: false,
-//!     dim: false,
-//!     italic: false,
-//!     underline: false
-//! };
-//! // Step 2
+//! // Step 1: Predefine Your Styles using the builder pattern
+//! let title_style = Style::builder()
+//!     .foreground(Some(Color::Green))
+//!     .background(Some(Color::Red))
+//!     .underline()
+//!     .build();
+//!
+//! // Step 2: Create Colored Strings
 //! let title_text: ColoredString = ColoredString::new(
 //!     "Hello World",
-//!     TITLESTYLE
+//!     title_style,
 //! );
 //!
-//! // Step 3
+//! // Step 3: Print the Colored String
 //! println!("{}", title_text);
 //! ```
-
+//!
+//! Please make sure your terminal supports ANSI colors by using the [`is_color_available`](crate::ansi_base::is_color_available) function before attempting to print colored text.
+//!
 mod ansi_base;
 pub mod types;
 
