2ff17805303d48acb16897a7250d43fa6a41cec1
SkuldNorniern <skuldnorniern@gmail.com>
2023-09-14 09:32:21 +0900

    style: remove `dead_code`

--- src/ansi_base.rs
+++ src/ansi_base.rs
@@ -1,20 +1,14 @@
-#[allow(dead_code)]
 pub const RESET: &str = "\x1b[0m";
-#[allow(dead_code)]
 pub const BOLD: &str = "\x1b[1m";
-#[allow(dead_code)]
 pub const DIM: &str = "\x1b[2m";
-#[allow(dead_code)]
 pub const ITALIC: &str = "\x1b[3m";
-#[allow(dead_code)]
 pub const UNDERLINE: &str = "\x1b[4m";
 
-#[allow(dead_code)]
-pub fn check_color_available() -> Result<&'static str, &'static str> {
-    //! Ckeck if the terminal supports ANSI colors
-
+/// Check if the terminal supports ANSI colors
+pub fn is_color_available() -> Result<(), &'static str> {
     if std::env::var("TERM").is_ok() {
-        return Ok("Terminal supports ANSI colors");
+        return Ok(());
     }
     Err("Terminal does not support ANSI colors")
 }
+
