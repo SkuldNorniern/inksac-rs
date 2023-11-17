a387e6d4ef1f943d9115171db0bac44060713514
WieeRd <verywieerd@gmail.com>
2023-11-17 23:41:39 +0900

    chore: mark some to-be-fixed spots

--- src/ansi_base.rs
+++ src/ansi_base.rs
@@ -4,6 +4,7 @@ pub const DIM: &str = "\x1b[2m";
 pub const ITALIC: &str = "\x1b[3m";
 pub const UNDERLINE: &str = "\x1b[4m";
 
+// FIX!: ASAP: what the actual fucking fuck just return boolean
 /// Check if the terminal supports ANSI colors
 pub fn is_color_available() -> Result<(), &'static str> {
     if std::env::var("TERM").is_ok() {
